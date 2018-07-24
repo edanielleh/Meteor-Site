4f7956ba6f96d7cfd169695d4f70d8f7a1aeac22
Danielle Howard <edanielleh@gmail.com>
2018-07-24 14:14:57 -0600

    Buttons Added

--- imports/components/introduction.jsx
+++ imports/components/introduction.jsx
@@ -6,5 +6,9 @@ export let introduction =
 <div className = 'intro-body'>
     <div className = 'intro-image'>
     <h1 className = 'intro-h1'>Danielle Howard <br/> Fullstack Developer</h1>
+    <div className = 'intro-btn-row'>
+        <button className = 'intro-linkedin'>LinkedIn</button>
+        <button className = 'intro-github'>GitHub</button>
+    </div>
     </div>
 </div>
